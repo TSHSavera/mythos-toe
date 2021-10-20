0b64ce701f8847f06367ead7093ee966cba4d25a
Sandren Troy Milante <sandrentroym@gmail.com>
2021-10-20 16:04:06 +0800

    3 new reworks/features
    The next-prev system
    New element data structure
    Console

---
+++ js/elementdata.js
@@ -0,0 +1,217 @@
+//elementdata.js
+//Author: Troy Milante
+
+//Colors
+const ed_cc_nm = "#F4B400";
+const ed_cc_am = "#DB3E23";
+const ed_cc_aem = "#4285F4";
+const ed_cc_tm = "#537EC5";
+const ed_cc_ptm = "#0F9D58";
+const ed_cc_m = "rgb(100,202,5)";
+const ed_cc_ng = "#E2644E";
+const ed_cc_h = "#972714";
+const ed_cc_l = "#CBAF71";
+const ed_cc_a = "#FAB647";
+
+
+const elementDatas = [
+    {
+        name: "Hydrogen",
+        symbol: "H",
+        atomconfig: "1s<sup>1</sup>",
+        cgb: "Non-Metal",
+        atomnos: "1",
+        atommass: "1.008 units",
+        color: ed_cc_nm,
+    },
+
+    {
+        name: "Helium",
+        symbol: "He",
+        atomconfig: "1s<sup>2</sup>",
+        cgb: "Noble Gas",
+        atomnos: "2",
+        atommass: "4.003 units",
+        color: ed_cc_ng,
+    },
+
+    {
+        name: "Lithium",
+        symbol: "Li",
+        atomconfig: "[He]2s<sup>1</sup>",
+        cgb: "Alkali Metals",
+        atomnos: "3",
+        atommass: "6.94 units",
+        color: ed_cc_am,
+    },
+
+    {
+        name: "Berrylium",
+        symbol: "Be",
+        atomconfig: "[He]2s<sup>2</sup>",
+        cgb: "Alkaline Earth Metals",
+        atomnos: "4",
+        atommass: "9.012 units",
+        color: ed_cc_aem,
+    },
+
+    {
+        name: "Boron",
+        symbol: "B",
+        atomconfig: "[He]2s<sup>2</sup>2p<sup>1</sup>",
+        cgb: "Metalloid",
+        atomnos: "5",
+        atommass: "10.81 units",
+        color: ed_cc_m,
+    },
+    
+    {
+        name: "Carbon",
+        symbol: "C",
+        atomconfig: "[He]2s<sup>2</sup>2p<sup>2</sup>",
+        cgb: "Non-Metal",
+        atomnos: "6",
+        atommass: "12.011	units",
+        color: ed_cc_nm,
+    },
+
+    {
+        name: "Nitrogen",
+        symbol: "N",
+        atomconfig: "[He]2s<sup>2</sup>2p<sup>3</sup>",
+        cgb: "Non-Metal",
+        atomnos: "7",
+        atommass: "14.007	units",
+        color: ed_cc_nm,
+    },
+    
+    {
+        name: "Oxygen",
+        symbol: "O",
+        atomconfig: "[He]2s<sup>2</sup>2p<sup>4</sup>",
+        cgb: "Non-Metal",
+        atomnos: "8",
+        atommass: "15.999	units",
+        color: ed_cc_nm,
+    },
+    
+    {
+        name: "Fluorine",
+        symbol: "F",
+        atomconfig: "[He]2s<sup>2</sup>2p<sup>5</sup>",
+        cgb: "Halogen",
+        atomnos: "9",
+        atommass: "18.998	units",
+        color: ed_cc_h,
+    },
+    
+    {
+        name: "Neon",
+        symbol: "Ne",
+        atomconfig: "[He]2s<sup>2</sup>2p<sup>6</sup>",
+        cgb: "Noble Gas",
+        atomnos: "10",
+        atommass: "20.180 units",
+        color: ed_cc_ng,
+    },
+    
+    {
+        name: "Sodium",
+        symbol:"Na",
+        atomconfig: "[Ne]3s<sup>1</sup>",
+        cgb: "Alkali Metals",
+        atomnos: "11",
+        atommass: "22.990 units",
+    },
+    
+    {
+        name: "Magnesium",
+        symbol: "Mg",
+        atomconfig: "[Ne]3s<sup>2</sup>",
+        cgb: "Noble Gas",
+        atomnos: "12",
+        atommass: "24.305 units",
+        
+    },
+    
+    {
+        name: "Aluminum",
+        symbol: "Al",
+        atomconfig: "[Ne]3s<sup>2</sup>3p<sup>1</sup>",
+        cgb: "Post-Transition Metal",
+        atomnos: "13",
+        atommass: "26.982 units",
+        
+    },
+    
+    {
+        name: "Silicon",
+        symbol: "Si",
+        atomconfig: "[Ne]3s<sup>2</sup>3p<sup>2</sup>",
+        cgb: "Metalloid",
+        atomnos: "14",
+        atommass: "28.085 units",
+        
+    },
+    
+    {
+        name: "Phosphorus",
+        symbol: "P",
+        atomconfig: "[Ne]3s<sup>2</sup>3p<sup>3</sup>",
+        cgb: "Non-Metal",
+        atomnos: "15",
+        atommass: "30.974 units",
+        
+    },
+    
+    {
+        name: "Sulfur",
+        symbol: "S",
+        atomconfig: "[Ne]3s<sup>2</sup>3p<sup>4</sup>",
+        cgb: "Non-Metal",
+        atomnos: "16",
+        atommass: "32.06 units",
+        
+    },
+    
+    {
+        name: "Chlorine",
+        symbol: "Cl",
+        atomconfig: "[Ne]3s<sup>2</sup>3p<sup>5</sup>",
+        cgb: "Halogen",
+        atomnos: "17",
+        atommass: "35.45 units",
+        
+    },
+    
+    {
+        name: "Argon",
+        symbol: "Ar",
+        atomconfig: "[Ne]3s<sup>2</sup>3p<sup>6</sup>",
+        cgb: "Noble Gas",
+        atomnos: "18",
+        atommass: "39.95 units",
+
+    },
+
+    {
+        name: "Potassium",
+        symbol: "K",
+        atomconfig: "[Ar]4s<sup>1</sup>",
+        cgb: "Alkali Metals",
+        atomnos: "19",
+        atommass: "39.098 units",
+        
+    },
+
+    {
+        name: "Calcium",
+        symbol: "Ca",
+        atomconfig: "[Ar]4s<sup>2</sup>",
+        cgb: "Alkaline Earth Metals",
+        atomnos: "20",
+        atommass: "40.078 units",
+
+    },
+
+]
